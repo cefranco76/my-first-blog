220e65755e08ccdbfee8cad730b8ffff7b6ef012
Eduardo Franco <cefranco@gmail.com>
2019-03-08 18:43:16 -0300

    Blog pronto com Form

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 #from django.contrib import session
+from .models import Post
 from .models import TabUsuario
 from .models import TabEventos
 from .models import TabImagens
 
+admin.site.register(Post)
 admin.site.register(TabUsuario)
 admin.site.register(TabEventos)
 admin.site.register(TabImagens)
