fa9f81bacf1f8edb0b5caf09b57bb6ccf003e82f
Eduardo Franco <cefranco@gmail.com>
2019-03-10 22:39:03 -0300

    Agora com login e logout

--- blog/urls.py
+++ blog/urls.py
@@ -13,7 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
+from django.urls import path, include
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from . import views
 
 
@@ -22,5 +24,10 @@ urlpatterns = [
 	path('post/<int:pk>/', views.post_detail, name='post_detail'),
 	path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 	path('post/new', views.post_new, name='post_new'),
+	path('post/<int:pk>/publish', views.post_publish, name='post_publish'),
+	path('post/<int:pk>/remove', views.post_remove, name='post_remove'),
+	path('drafts/', views.post_draft_list, name='post_draft_list'),
+	path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+	path('accounts/logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 	
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404,redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+
 #from .models import TabUsuario
 #from .models import TabImagens
 
@@ -16,42 +18,62 @@ def post_list(request):
     posts = Post.objects.order_by('published_date') 
     return render(request, 'blog/post_list.html', {'posts':posts})     
      
+def post_draft_list(request): 
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date') 
+    return render(request, 'blog/post_draft_list.html', {'posts':posts})     
+ 
 def post_detail(request,pk): 
     post = Post.objects.get(pk=pk)
     return render(request, 'blog/post_detail.html', {'post':post})     
 
-def post_edit(request,pk): 
-    post = Post.objects.get(pk=pk)
-    return render(request, 'blog/post_edit.html', {'post':post})
+#def post_edit(request,pk): 
+#    post = Post.objects.get(pk=pk)
+#    return render(request, 'blog/post_edit.html', {'post':post})
 
+@login_required
 def post_new(request):
     if request.method == "POST":
-       form = PostForm(request.POST)
-       if form.is_valid():
-             post = form.save(commit=False)
-             post.author = request.user
-             post.published_date = timezone.now()
-             post.save()
-             return redirect('post_detail', pk=post.pk)
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            #post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
     else:
-       form = PostForm()
+        form = PostForm()
     
     return render(request, 'blog/post_edit.html', {'form': form})
 
+@login_required
 def post_edit(request, pk):
-     post = get_object_or_404(Post, pk=pk)
-     if request.method == "POST":
-         form = PostForm(request.POST, instance=post)
-         if form.is_valid():
-             post = form.save(commit=False)
-             post.author = request.user
-             post.published_date = timezone.now()
-             post.save()
-             return redirect('post_detail', pk=post.pk)
-     else:
-         form = PostForm(instance=post)
-     return render(request, 'blog/post_edit.html', {'form': form})
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            #post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
 
-     
+@login_required
+def post_publish(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    post.publish()
+    return redirect('post_list')
+
+@login_required
+def post_remove(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    post.delete()
+    return redirect('post_list')
+	
+
+
+	
      
 
